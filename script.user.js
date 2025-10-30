2e8c891bd3c2cf8b6975c2cc98eebffe2783a313
badtothebone430 <lokiincharge25@gmail.com>
2025-10-30 16:54:34 -0400

    Create SimCompanies Premium user script
    Initial creation of a Tampermonkey user script for SimCompanies.

---
+++ script.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         SimCompanies Premium
+// @version      1.0.0
+// @namespace    http://tampermonkey.net/
+// @description  Enhancements for SimCompanies web game. Complies with scripting rules of the game.
+// @author       Loki Clarke
+// @match        https://www.simcompanies.com/*
+// @require      https://raw.githubusercontent.com/badtothebone430/chartjs/main/shongi.js
+// @require      https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns@3.0.0/dist/chartjs-adapter-date-fns.bundle.min.js
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @connect      simcotools.app
+// @run-at       document-start
+// @license MIT
+
+// ==/UserScript==
+
